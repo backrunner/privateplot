ad3656abf4859752fd9d7167569302911a25c7c2
BackRunner <dev@backrunner.top>
2025-01-23 01:27:50 +0800

    feat: add footer and tune styles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'BackRunner - Private Plot';
+export const SITE_TITLE = 'BackRunner\'s Plot';
 export const SITE_DESCRIPTION = 'Turn ideas into codes and bring them to reality';
+export const ENABLE_COPYRIGHT = true;
+export const ENABLE_POWERED_BY = true;
